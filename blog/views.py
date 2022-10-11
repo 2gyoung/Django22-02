c7f085a98350a3a45aa74adf923ebee18f161c52
GaYoung <leegyoung12@gmail.com>
2022-10-11 15:05:53 +0900

    0927 InternetProgramming

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
+def index(request):
+    posts1 = Post.objects.all().order_by('-pk')
+    return render(request, 'blog/index.html', {'posts2': posts1})
+
+def single_post_page(request, pk):
+    post3 = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {'post':post3})
