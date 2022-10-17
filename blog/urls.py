2b60ae5919dc9a07aa1b312f4afee85ba09cc6a4
GaYoung <leegyoung12@gmail.com>
2022-10-17 21:25:24 +0900

    FBV로 포스트 목록/상세 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [ #IP주소/blog
   path('', views.index), #IP주소/blog
-  path('<int:pk>', views.single_post_page)
+  path('<int:pk>/', views.single_post_page)
 ]
